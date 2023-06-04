4da31de8b54d72141de9384b57e6817148c326c6
baris-tunc <baristunc00@hotmail.com>
2023-06-05 02:57:50 +0300

    test

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,23 +1,25 @@
-const swiper = new Swiper('.customSlider', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 10,
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 3,
-            spaceBetween: 30
-        },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 4,
-            spaceBetween: 40
+window.addEventListener('load', function() {
+    const swiper = new Swiper('.customSlider', {
+        // Default parameters
+        slidesPerView: 1,
+        spaceBetween: 10,
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 40
+            }
         }
-    }
+    })
 })
